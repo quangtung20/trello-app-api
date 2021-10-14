0d54ac0c3a4f4588af400396b512af3b94401190
quangtung20 <trangquangtung20111999@gmail.com>
2021-10-14 16:51:17 +0700

    design db

---
+++ src/models/card.model.js
@@ -0,0 +1,30 @@
+import Joi from 'joi';
+import { getDB } from '../config/mongodb';
+//define card colection
+
+const cardCollectionName = 'cards';
+const cardCollectionSchema = Joi.object({
+    boardId: Joi.string().required(),
+    columnId: Joi.string().required(),
+    title: Joi.string().required().min(3).max(30),
+    cover: Joi.string().default(null),
+    createdAt: Joi.date().timestamp().default(Date.now()),
+    updatedAt: Joi.date().timestamp().default(null),
+    _destroy: Joi.boolean().default(false),
+})
+
+const validateSchema = async (data) => {
+    return await cardCollectionSchema.validateAsync(data, { abortEarly: false });//log ra het cac loi
+}
+
+const createNew = async (data) => {
+    try {
+        const value = await validateSchema(data);
+        const result = await getDB().collection(cardCollectionName).insertOne(value);
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const CardModel = { createNew }
