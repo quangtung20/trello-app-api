d90c0950341eb54091120e7832316b8ac6ebe3b0
quangtung20 <trangquangtung20111999@gmail.com>
2021-10-22 22:21:08 +0700

    crud column

--- src/services/column.service.js
+++ src/services/column.service.js
@@ -1,10 +1,12 @@
 import { columnModel } from '*/models/column.model.js';
 import { boardModel } from '*/models/board.model.js';
+import { cardModel } from '*/models/card.model.js';
 
 
 let createNew = async (data) => {
     try {
         const newColumn = await columnModel.createNew(data);
+        newColumn.cards = [];
         // update column order array in board
         await boardModel.pushColumnOrder(newColumn.boardId.toString(), newColumn._id.toString());
         return newColumn;
@@ -19,8 +21,14 @@ let update = async (id, data) => {
             ...data,
             updatedAt: Date.now(),
         }
-        const result = await columnModel.update(id, updateData);
-        return result;
+        if (updateData._id) delete updateData._id;
+        if (updateData.cards) delete updateData.cards;
+        const updatedColumn = await columnModel.update(id, updateData);
+        if (updatedColumn._destroy) {
+            // delete many cards in column
+            cardModel.deleteMany(updatedColumn.cardOrder);
+        }
+        return updatedColumn;
     } catch (error) {
         throw new Error(error);
     }
